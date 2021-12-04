593d014ca3c01c0778451c88183b3eb54009a98f
Boris Grigorov <boris.grigorov26@gmail.com>
2021-12-05 00:09:30 +0100

    Flashing

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-import './style.css'
+import "./style.css";
+
+var body = document.querySelector("body");
+var text = document.querySelector("#app");
+setInterval(() => {
+    randomColorFlash();
+    setTimeout(() => {
+        body.style.backgroundColor = "#0b0e21";
+        text.style.color = "white";
+    }, randomTime(50, 150));
+    setTimeout(() => {
+        randomColorFlash();
+        setTimeout(() => {
+            body.style.backgroundColor = "#0b0e21";
+            text.style.color = "white";
+        }, randomTime(20, 80));
+    }, randomTime(150, 250));
+}, randomTime(3000, 7000));
+
+function randomColorFlash() {
+    body.style.backgroundColor = "white";
+    text.style.color = "#0b0e21";
+}
+function randomTime(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
